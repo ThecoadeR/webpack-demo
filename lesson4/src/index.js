04338fd05766a9297e757db9f5b69e99e367baec
Zhu <406132631@qq.com>
2020-03-02 16:06:15 +0800

    新增hot module replacement

--- lesson4/src/index.js
+++ lesson4/src/index.js
@@ -2,6 +2,15 @@
  * @Descripttion: 
  * @Author: Zhu Hai Hua
  * @Date: 2020-03-02 14:38:27
- * @LastEditTime: 2020-03-02 14:50:27
+ * @LastEditTime: 2020-03-02 15:15:16
  */
-console.log('test!!!!!!')
+import './index.css'
+
+var btn = document.createElement('button')
+btn.innerHTML = '点击'
+document.body.appendChild(btn)
+btn.onclick = function () {
+  var div = document.createElement('div')
+  div.innerHTML = '新增的div'
+  document.body.appendChild(div)
+}
--- lesson4/webpack.config.js
+++ lesson4/webpack.config.js
@@ -2,11 +2,12 @@
  * @Descripttion: 
  * @Author: Zhu Hai Hua
  * @Date: 2020-03-02 14:38:06
- * @LastEditTime: 2020-03-02 15:04:28
+ * @LastEditTime: 2020-03-02 15:20:19
  */
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   mode: 'development',
@@ -14,17 +15,28 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[contentHash:8].js'
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
   devServer: {
     contentBase: './dist', // 当前服务起在哪个路径下(打包后的dist文件夹下)
     open: true, // 自动打开浏览器
-    port: 8888 // 端口
+    port: 8888, // 端口
+    hot: true,
+    hotOnly: true
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ]
 }
